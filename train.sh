a0335845e9f2cc3214e99b10da0cca8c75676224
AlbertiPot <1071794833@qq.com>
2021-06-02 16:40:33 +0800

    reprofile the diretory

---
+++ train.sh
@@ -0,0 +1,27 @@
+# use pytorch-lighting
+# nohup \
+# /home/gbc/.conda/envs/nanodet/bin/python tools/train.py nanodet-m.yml \
+# &
+############ not convergence
+
+# tools/deprecated/train.py with single gpu
+nohup \
+/home/gbc/.conda/envs/nanodet/bin/python \
+tools/deprecated/train.py \
+mytrain_coco_bs192_lr14_280eps_gpu1/nanodet-m.yml \
+&
+
+# tensorboard --logdir=/home/gbc/workspace/nanodet/mytrain_coco_bs192_lr14_280eps_gpu1 --port=10001 --bind_all &
+
+
+# tools/deprecated/train.py with distributed training
+# nohup \
+# /home/gbc/.conda/envs/nanodet/bin/python \
+# -m torch.distributed.launch \
+# --nproc_per_node=2 \
+# --master_port 29501 \
+# tools/deprecated/train.py \
+# mytrain_coco_bs96_lr14_280eps_gpu2_distri/nanodet-m.yml \
+# &
+
+# tensorboard --logdir=/home/gbc/workspace/nanodet/mytrain_coco_bs96_lr14_280eps_gpu2_distri --port=10002 --bind_all &
