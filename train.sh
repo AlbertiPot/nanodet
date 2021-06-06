7ecca44bdf2e30e29ba5fb124e862e9a4aa6a479
AlbertiPot <1071794833@qq.com>
2021-06-06 14:12:54 +0800

    annotation

--- train.sh
+++ train.sh
@@ -1,26 +1,26 @@
-# use pytorch-lighting
+# use pytorch-lighting with distributed training
 # nohup \
 # /home/gbc/.conda/envs/nanodet/bin/python tools/train.py nanodet-m.yml \
 # &
 ############ not convergence
 
 # tools/deprecated/train.py with single gpu
-nohup \
-/home/gbc/.conda/envs/nanodet/bin/python \
-tools/deprecated/train.py \
-mytrain_coco_bs192_lr14_280eps_gpu1/nanodet-m.yml \
-&
-
-# tensorboard --logdir=/home/gbc/workspace/nanodet/mytrain_coco_bs192_lr14_280eps_gpu1 --port=10001 --bind_all &
-
-# tools/deprecated/train.py with distributed training
 # nohup \
 # /home/gbc/.conda/envs/nanodet/bin/python \
-# -m torch.distributed.launch \
-# --nproc_per_node=2 \
-# --master_port 29501 \
 # tools/deprecated/train.py \
-# mytrain_coco_bs96_lr14_280eps_gpu2_distri/nanodet-m.yml \
+# mytrain_coco_bs192_lr14_280eps_gpu1/nanodet-m.yml \
 # &
+# tensorboard --logdir=/home/gbc/workspace/nanodet/mytrain/coco_bs192_lr14_280eps_gpu1 --port=10001 --bind_all &
+############ convergence mAP0.2047 vs 20.6 announced
 
-# tensorboard --logdir=/home/gbc/workspace/nanodet/mytrain_coco_bs96_lr14_280eps_gpu2_distri --port=10002 --bind_all &
+# tools/deprecated/train.py with distributed training
+nohup \
+/home/gbc/.conda/envs/nanodet/bin/python \
+-m torch.distributed.launch \
+--nproc_per_node=2 \
+--master_port 29501 \
+tools/deprecated/train.py \
+mytrain/coco_bs96mul2_lr14_280eps_gpu2_distri/nanodet-m.yml \
+&
+# nohup tensorboard --logdir=/home/gbc/workspace/nanodet/mytrain/coco_bs96mul2_lr14_280eps_gpu2_distri --port=10001 --bind_all &
+############ convergence mAP0.2034 vs 20.6 announced
